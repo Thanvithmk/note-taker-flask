554bc87b39ddcde5fc9b9c131f31b39b0f6985ba
User3 <user3@gmail.com>
2025-11-23 12:28:00 +0530

    Final commit

--- website/auth.py
+++ website/auth.py
@@ -50,7 +50,7 @@ def signup():
             new_user=User(email=email,first_name=firstname,password=generate_password_hash(password1))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user,remember=True)
+            login_user(new_user,remember=True)
             flash('Account created!',category='success')
             return redirect(url_for('views.home')) #\home is also acceptable
         
---
+++ website/static/index.js
@@ -0,0 +1,5 @@
+function deleteNote(noteId){
+    fetch('/delete-note',{
+        
+    })
+}
