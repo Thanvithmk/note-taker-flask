e19e0912b8044959ce5048fc565efb108f2ce697
User3 <user3@gmail.com>
2025-11-23 11:46:32 +0530

    login logout home filters working fine

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,8 @@ from flask import Blueprint,render_template,request,flash,redirect,url_for
 from .models import User
 from werkzeug.security import generate_password_hash,check_password_hash
 from . import db
+from flask_login import login_user,login_required,logout_user,current_user
+
 
 auth=Blueprint('auth',__name__)
 
@@ -15,17 +17,14 @@ def login():
         if user:
             if check_password_hash(user.password,password):
                 flash('Logged in successfully!',category='success')
+                login_user(user,remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.',category='error')
+        else:
+            flash("Not registered",category='error')
+    return render_template('login.html',user=current_user)
 
-
-
-    return render_template('login.html',text="Hello from login route")
-
-@auth.route("/logout")
-def logout():
-    return render_template('logout.html')
 
 @auth.route("/signup",methods=['GET','POST'])
 def signup():
@@ -51,10 +50,15 @@ def signup():
             new_user=User(email=email,first_name=firstname,password=generate_password_hash(password1))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user,remember=True)
             flash('Account created!',category='success')
             return redirect(url_for('views.home')) #\home is also acceptable
         
-        
+    return render_template('signup.html',user=current_user)
 
-    return render_template('signup.html')
 
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
