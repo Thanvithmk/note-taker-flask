564233d6c2a45eea4377661d4ecc0bbd802b25a3
User3 <user3@gmail.com>
2025-11-23 11:29:20 +0530

    login and signup working

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,26 @@
-from flask import Blueprint,render_template,request,flash
+from flask import Blueprint,render_template,request,flash,redirect,url_for
+from .models import User
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import db
 
 auth=Blueprint('auth',__name__)
 
 @auth.route("/login",methods=['GET','POST'])
 def login():
+    if request.method=='POST':
+        email=request.form.get('email')
+        password=request.form.get('password')
+
+        user=User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password,password):
+                flash('Logged in successfully!',category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.',category='error')
+
+
+
     return render_template('login.html',text="Hello from login route")
 
 @auth.route("/logout")
@@ -19,7 +36,10 @@ def signup():
         password1=request.form.get('password1')
         password2=request.form.get('password2')
 
-        if len(email)<4:
+        user=User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists',category='error')
+        elif len(email)<4:
             flash('Email must be greater than 4 characters.',category='error')
         elif len(password1)<7:
             flash('Password must be at least 7 characters.',category='error')
@@ -28,9 +48,13 @@ def signup():
         elif len(firstname)<2:
             flash('First name must be at least 2 characters.',category='error')
         else:
+            new_user=User(email=email,first_name=firstname,password=generate_password_hash(password1))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!',category='success')
+            return redirect(url_for('views.home')) #\home is also acceptable
         
-        print(email,password1,password2)
+        
 
     return render_template('signup.html')
 
